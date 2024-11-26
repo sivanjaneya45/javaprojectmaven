7dd3a2bd34ad9f671a485d26f1ec7dceee92435d
𝕤𝕚𝕧𝕒𝕟𝕛𝕒𝕟𝕖𝕪 <HP@sivanjaneya.sivanjanyeya>
2024-11-26 22:40:28 +0530

    Your descriptive commit message here

---
+++ src/main/java/com/example/sample_j_demo/App.java
@@ -0,0 +1,10 @@
+package com.example.sample_j_demo;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
